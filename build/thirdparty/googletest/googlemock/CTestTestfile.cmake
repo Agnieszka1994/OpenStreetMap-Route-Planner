75f1976561edbad2ea20af23fbce2b00fa55988f
Agnieszka <agnieszkalasota1994@gmail.com>
2020-12-21 11:38:14 +0100

    :fire: build deleted from git

--- build/thirdparty/googletest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/agnieszka/dev/RoutePlanner/OpenStreetMap-Route-Planner/thirdparty/googletest/googlemock
-# Build directory: /home/agnieszka/dev/RoutePlanner/OpenStreetMap-Route-Planner/build/thirdparty/googletest/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
--- build/thirdparty/googletest/googlemock/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/agnieszka/dev/RoutePlanner/OpenStreetMap-Route-Planner/thirdparty/googletest/googletest
-# Build directory: /home/agnieszka/dev/RoutePlanner/OpenStreetMap-Route-Planner/build/thirdparty/googletest/googlemock/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
