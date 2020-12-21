18cbb89bc1675261cc574a4411a474d2693e269b
Agnieszka <agnieszkalasota1994@gmail.com>
2020-12-21 11:32:48 +0100

    :fire: VS files deleted - cmake added for build

---
+++ build/thirdparty/googletest/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/agnieszka/dev/RoutePlanner/OpenStreetMap-Route-Planner/thirdparty/googletest
+# Build directory: /home/agnieszka/dev/RoutePlanner/OpenStreetMap-Route-Planner/build/thirdparty/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googlemock")
---
+++ build/thirdparty/googletest/googlemock/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/agnieszka/dev/RoutePlanner/OpenStreetMap-Route-Planner/thirdparty/googletest/googlemock
+# Build directory: /home/agnieszka/dev/RoutePlanner/OpenStreetMap-Route-Planner/build/thirdparty/googletest/googlemock
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
